d12b1fac4aa1225c9a2a1e7802b400992aaf66e4
atul <atulpatare99@gmail.com>
2021-06-09 20:53:18 +0530

    added examples

--- run.py
+++ run.py
@@ -4,22 +4,26 @@ from src import levenshtein_ratio_and_distance, Dictionary
 dictionary = Dictionary()
 
 # input from user
-input_str = input("Enter a string to compare :: ")
+inputs = [
+    "giraff is an animl",
+    "honesy is the best polic",
+]
 
 print("\n\n")
 print("-------------------------------------------------------")
 print("| Original Word |\t Dictionary Word \t|\t Ratio \t |")
 print("-------------------------------------------------------")
 
-for word in input_str.split():
-    most_matching, max_ratio = word, 0
+for string in inputs:
+    for word in string.split():
+        most_matching, max_ratio = word, 0
 
-    for dict_word in dictionary.words()[word[0]]:
-        ratio = levenshtein_ratio_and_distance(word, dict_word, ratio_calc=True)
+        for dict_word in dictionary.words()[word[0]]:
+            ratio = levenshtein_ratio_and_distance(word, dict_word, ratio_calc=True)
 
-        if ratio >= max_ratio:
-            max_ratio, most_matching = ratio, dict_word
+            if ratio >= max_ratio:
+                max_ratio, most_matching = ratio, dict_word
 
-    print(f"| {word: <20}  {most_matching: <20}  {round(max_ratio, 3): <8}  ")
-    print("-------------------------------------------------------")
+        print(f"| {word: <20}  {most_matching: <20}  {round(max_ratio, 3): <8}  ")
+        print("-------------------------------------------------------")
 
